4c7e421dfdcbac8c8a45b3bf209883e24e1db09c
shiva <shiva@HP>
2024-02-20 10:50:18 +0530

    Push main project

---
+++ ShoppingMart/src/test/java/com/example/demo/ShoppingMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
